27c8f52c2aecf24978b102b89bf5e5ddeb17097b
Harshvardhan <hv9796923@gmail.com>
2025-11-16 11:29:03 +0530

    Run cron-job every 6 hours

--- cron-jobs/cron-setup.js
+++ cron-jobs/cron-setup.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 export function sendCurrentBalanceNotificationCron (){
     // Schedule: “0 8 * * *” → every day at 08:00 AM
-    cron.schedule("0 8 * * *", async () => {
+    cron.schedule("0 */6 * * *", async () => {
   console.log("⏰ Running daily balance check at", new Date().toLocaleString());
 
   try {
