b5b0c50e758263f12ae377f17ed9459af04862cc
Harshvardhan <hv9796923@gmail.com>
2026-01-07 23:37:58 +0530

    set default navigation timeout to 60 seconds

--- cron-jobs/cron-setup.js
+++ cron-jobs/cron-setup.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 export function sendCurrentBalanceNotificationCron (){
     // Schedule: “0 8 * * *” → every day at 08:00 AM
-    cron.schedule("*/1 * * * *", async () => {
+    cron.schedule("0 */6 * * *", async () => {
   console.log("⏰ Running daily balance check at", new Date().toLocaleString());
 
   try {
@@ -16,21 +16,21 @@ export function sendCurrentBalanceNotificationCron (){
     console.log(`💡 Current balance: ₹${balance}`);
 
     if (balance < 50) {
-      // await sendEmail({
-      //   to: 'mytechacccount@gmail.com',
-      //   subject: "⚠️ Low Electricity Balance Alert",
-      //   text: `Your NBPDCL balance is ₹${balance}. Please recharge soon.`,
-      // });
-      // console.log("📩 Alert email sent");
+      await sendEmail({
+        to: 'mytechacccount@gmail.com',
+        subject: "⚠️ Low Electricity Balance Alert",
+        text: `Your NBPDCL balance is ₹${balance}. Please recharge soon.`,
+      });
+      console.log("📩 Alert email sent");
     } else {
       console.log("✅ Balance above threshold, no alert sent.");
     }
   } catch (err) {
-    // await sendEmail({
-    //     to: 'mytechacccount@gmail.com',
-    //     subject: "⚠️ ERROR Fetching Electricity Balance",
-    //     text: `There was an error fetching your NBPDCL balance: ${err}`,
-    //   });
+    await sendEmail({
+        to: 'mytechacccount@gmail.com',
+        subject: "⚠️ ERROR Fetching Electricity Balance",
+        text: `There was an error fetching your NBPDCL balance: ${err}`,
+      });
     console.error("❌ Error fetching balance or sending mail:", err);
   }
 }, {timezone: "Asia/Kolkata"});
